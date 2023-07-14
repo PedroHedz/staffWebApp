5ffc536a01e8aed71b7010d96eecf8dcd63a9858
Pedro Hernandez <pitermashup@hotmail.com>
2023-07-14 11:12:39 -0500

    Correccion

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,9 +29,9 @@ const RESOURCES = {"assets/AssetManifest.json": "6e0037832f653af73df277c225c1fd0
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "24caa0fe95136f75c4409ce111d40a2b",
-"/": "24caa0fe95136f75c4409ce111d40a2b",
-"main.dart.js": "a93053ddff826af836204b0cbd63d270",
+"index.html": "dd27e5da5b517d558edb9592b6516301",
+"/": "dd27e5da5b517d558edb9592b6516301",
+"main.dart.js": "1f33a5c15e8b1481a67c165897974ad9",
 "manifest.json": "670e9c42ccaab5021b8a6202dfc870d9",
 "version.json": "9892006dcc28c28d2c00194e1f173ced"};
 // The application shell files that are downloaded before a service worker can
